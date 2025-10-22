e0b1fa87ebc76e2c7a2de89efec85d1a48aa281a
congquan92 <145172432+congquan92@users.noreply.github.com>
2025-10-22 17:37:16 +0700

    ok

--- client/src/components/NoChatSelected.tsx
+++ client/src/components/NoChatSelected.tsx
@@ -1,4 +1,4 @@
-import { MessageSquare } from "lucide-react";
+import { MessageCircle } from "lucide-react";
 
 export default function NoChatSelected() {
     return (
@@ -8,13 +8,13 @@ export default function NoChatSelected() {
                 <div className="flex justify-center gap-4 mb-4">
                     <div className="relative">
                         <div className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center justify-center animate-bounce">
-                            <MessageSquare className="w-8 h-8 text-primary " />
+                            <MessageCircle className="w-8 h-8 text-primary " />
                         </div>
                     </div>
                 </div>
 
                 {/* Welcome Text */}
-                <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+                <h2 className="text-2xl font-bold">Welcome to ChatWeb</h2>
                 <p className="text-base-content/60">Select a conversation from the sidebar to start chatting</p>
             </div>
         </div>
